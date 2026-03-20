a059f6b4f8f578b4aadc6ddbab24a05a0f954b52
BotsByLarry <advaitpathak27@proton.me>
2026-03-20 14:42:14 +0530

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-const WEBHOOKS = {
-    "rules": "https://discord.com/api/webhooks/1484475209678589974/FGBqYkFAiwplOLo_Rub1tJ8IRwZrXNf5qrm4-54vx-OEUE5Dms77hFXkt1Mn6G2UAsVk",
-    "announcements": "https://discord.com/api/webhooks/1484477711895236648/54uanRj5FL0wB4S4eFpLa0ag07qltbqrm0r80slOSPWh2qlRc0Ul-qrm9ymK9aqRunbX",
-    "events": "https://discord.com/api/webhooks/1484477416280948746/GeoBMiFsIY5c7kIqBgYwt0Huo3CFj6XpsZuU4LDSSd1Xck7-oECbvU44qaM5DpaZHku6",
-    "jb-shop": "https://discord.com/api/webhooks/1484477069998952541/zeiF3XF_Q6ueeUaLx5d89gsMKWQXuGQguNZIZPDPNCrGtD4QJ3otrP-_9sMKsGR5_RgR",
-    "other-shops": "https://discord.com/api/webhooks/1484477277768257566/4hElATIyKFu1OJKDlMP0lGiCXKvmYe9RpZo8ETrkwtarN9f6uTiFaXUMoGCS3iAxdD2n
-"
-};
